244bc560ba4853036ac9ab772afe1a768832149a
BOOK-GJUJSUS23D\suhye <1265asha38@gmail.com>
2023-10-05 20:08:11 +0900

    코드 수정

--- day4.py
+++ day4.py
@@ -1,17 +1,18 @@
 playing = True
 
 while playing:      #True일때 계속 반복
-    first_num = int(input("choose a number:\n"))    #첫번째 수 입력
-    second_num = int(input("choose a another one:\n"))  #두번째 수 입력
-    ops = input("choose an operation:\n    Options are: +, - , * or /.\n    Write 'exit' to finish.\n") #연산자 또는 exit 입력
-    if ops == "+":      #연산자가 +이면 덧셈결과 출력
-        print(first_num + second_num)
-    elif ops == "-":    #연산자가 -이면 뺄셈결과 출력
-        print(first_num - second_num)
-    elif ops == "*":    #연산자가 *이면 곱셈결과 출력
-        print(first_num * second_num)
-    elif ops == "/":    #연산자가 /이면 나눗셈결과 출력
-        print(first_num / second_num)
+    a = int(input("Choose a number:\n"))
+    b = int(input("Choose another one:\n"))
+    operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
+    if operation == "+":      #연산자가 +이면 덧셈결과 출력
+        print(a + b)
+    elif operation == "-":    #연산자가 -이면 뺄셈결과 출력
+        print(a - b)
+    elif operation == "*":    #연산자가 *이면 곱셈결과 출력
+        print(a * b)
+    elif operation == "/":    #연산자가 /이면 나눗셈결과 출력
+        print(a / b)
     else:               #다 아니면 종료
         playing = False
+
     
